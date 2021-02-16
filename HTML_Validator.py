999c143833e4341cca5fd0de939904982782c190
samcbogen <Samuel.Bogen.23@cmc.edu>
2021-02-15 22:25:26 -0800

    flakey

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -3,21 +3,21 @@
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited 
+    version of html validation by checking
+    whether every opening tag has a corresponding closing tag.
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
     False
     '''
-
-
     # HINT:
-    # use the _extract_tags function below 
+    # use the _extract_tags function below
     # to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced 
+    # then process these html tags using the balanced
     # parentheses algorithm from the class/book
-    # the main difference between your code and the 
-    # code from class will be that you will have to 
+    # the main difference between your code and the
+    # code from class will be that you will have to
     # keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
     tags = _extract_tags(html)
@@ -42,16 +42,13 @@ def validate_html(html):
                 return False
     return len(stack) == 0
 
-
-
-
-
 def _extract_tags(html):
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be 
+    By convention in Python, helper functions that are not meant to be
     used directly by the user are prefixed with an underscore.
-    This function returns a list of all the html tags contained in the input string,
+    This function returns a list
+    of all the html tags contained in the input string,
     stripping out all text not contained within angle brackets.
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
@@ -66,4 +63,3 @@ def _extract_tags(html):
                 b += 1
             extract.append(html[i:b+1])
     return extract
-
